2f80cd8da788addad0e1a8ec1bebc14eb05ca290
Aicee <codingaicee@gmail.com>
2024-01-03 00:22:07 +0800

    first commit

---
+++ top/webpack/editor-elements-common-utils/dist/esm/bi/analytics/const.js
@@ -0,0 +1,6 @@
+export const AnalyticsPromoteSrc = 76;
+export const AnalyticsPromoteEndpoint = 'pa';
+export const ElementsClick = 1113;
+export const AnalyticsClicksCollapsibleTextExpandActionName = 'Expand';
+export const AnalyticsClicksCollapsibleTextUDPExpandActionName = 'Expand';
+//# sourceMappingURL=consts.js.map
